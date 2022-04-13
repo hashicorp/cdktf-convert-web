23ba8b686bfd8df1a0a2e6a0b1b49c4bb0da5240
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-04-13 18:35:05 +0200

    feat: first deploying lambda

--- functions/convert/app.ts
+++ functions/convert/app.ts
@@ -1,7 +1,7 @@
 // const querystring = require("querystring");
 // const { convert } = require("@cdktf/hcl2cdk");
 
-function res(status, content) {
+function res(status: number, content: Record<string, any>) {
   return {
     statusCode: status,
     headers: {
@@ -14,7 +14,14 @@ function res(status, content) {
 
 // Handles requests like POST /?provider='aws%40%3D3.0'&provider='google%40%3D4.0.5'
 // Runs convert with post content and all mentioned providers
-exports.handler = async function (event, context) {
+exports.handler = async function (
+  event: Record<string, any>,
+  _context: unknown
+) {
+  if (!event.requestContext) {
+    return res(500, { error: "Expected Event Request Context to be set" });
+  }
+
   const {
     requestContext: { httpMethod, path },
   } = event;
--- main.ts
+++ main.ts
@@ -1,14 +1,75 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, TerraformOutput, TerraformStack } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws";
+import { Provider } from "cdktf-local-build";
+import { DockerFunction } from "@cdktf-plus/aws";
+import path = require("path");
+import { Apigatewayv2Api } from "@cdktf/provider-aws/lib/apigatewayv2";
+import { ExternalProvider } from "@cdktf/provider-external";
+import { LambdaPermission } from "@cdktf/provider-aws/lib/lambdafunction";
 
-class MyStack extends TerraformStack {
+class PrefixConstruct extends Construct {
+  protected prefix: string;
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    const stack = TerraformStack.of(this);
+
+    this.prefix = (stack as any).prefix;
+  }
+}
+
+const region = "eu-central-1";
+
+class ConvertLambda extends PrefixConstruct {
+  public endpoint: string;
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const handler = new DockerFunction(this, "handler", {
+      path: path.resolve(__dirname, "functions/convert"),
+    });
+
+    const apiEndpoint = new Apigatewayv2Api(this, "api", {
+      name: `${this.prefix}-convert-api`,
+      protocolType: "HTTP",
+      target: handler.fn.arn,
+      corsConfiguration: {
+        allowOrigins: ["*"],
+        allowMethods: ["*"],
+        allowHeaders: ["content-type"],
+      },
+    });
+    new LambdaPermission(this, "markhub-upload-auth-permission", {
+      statementId: "AllowApiGatewayHTTP",
+      action: "lambda:InvokeFunction",
+      functionName: handler.fn.arn,
+      principal: "apigateway.amazonaws.com",
+      sourceArn: `${apiEndpoint.executionArn}/*/*`,
+    });
+    this.endpoint = apiEndpoint.apiEndpoint;
+  }
+}
+
+class ConvertPage extends TerraformStack {
+  prefix: string;
   constructor(scope: Construct, name: string) {
     super(scope, name);
+    this.prefix = name;
 
-    // define resources here
+    new AwsProvider(this, "aws", {
+      region,
+    });
+    new Provider(this, "local");
+    new ExternalProvider(this, "external");
+
+    const lambda = new ConvertLambda(this, "convert");
+
+    new TerraformOutput(this, "convert-backend-url", {
+      value: lambda.endpoint,
+    });
   }
 }
 
 const app = new App();
-new MyStack(app, "convert-page");
+new ConvertPage(app, "development");
 app.synth();
